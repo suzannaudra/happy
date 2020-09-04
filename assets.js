48df3d1ecf31366c8a9d32a0dca4b6a1ab4aad2c
Suzann Kowalski <suzannaudra@gmail.com>
2020-09-03 20:38:15 -0700

    Initial Commit

---
+++ assets.js
@@ -0,0 +1,17 @@
+
+var nextWord = (function() {
+    var quote =  ["What we fear doing most is someting we need to do.", "What can I learn from this?",
+                "Everything is figreoutable",
+                "What's your end game?","Challenges are what make life interesting and overcoming them is what makes life meaningful."];
+var randomItem = quote[Math.floor(Math.random()*quote.length)];
+document.body.innerHTML = randomItem;
+})
+
+
+
+
+
+
+
+    
+  
