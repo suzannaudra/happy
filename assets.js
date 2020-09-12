e52419f1fbda3b7ccecb612631fedbf31043371a
Suzann Kowalski <suzannaudra@gmail.com>
2020-09-11 17:45:23 -0700

    Quotes on same page as button

--- assets.js
+++ assets.js
@@ -1,17 +1,11 @@
-
-var nextWord = (function() {
-    var quote =  ["What we fear doing most is someting we need to do.", "What can I learn from this?",
-                "Everything is figreoutable",
-                "What's your end game?","Challenges are what make life interesting and overcoming them is what makes life meaningful."];
-var randomItem = quote[Math.floor(Math.random()*quote.length)];
-document.body.innerHTML = randomItem;
-})
-
-
-
-
-
-
-
-    
-  
+var nextWord = function () {
+  var quote = [
+    "What we fear doing most is someting we need to do.",
+    "What can I learn from this?",
+    "Everything is figreoutable",
+    "What's your end game?",
+    "Challenges are what make life interesting and overcoming them is what makes life meaningful."
+  ];
+  var randomItem = quote[Math.floor(Math.random() * quote.length)];
+  document.getElementById("nextWord").innerHTML = randomItem;
+};
