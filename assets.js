4492686b56b03bfedb985bf262ed1085264f07cf
Suzann Kowalski <suzannaudra@gmail.com>
2020-09-12 21:07:53 -0700

    new quotes

--- assets.js
+++ assets.js
@@ -4,7 +4,8 @@ var nextWord = function () {
     "What can I learn from this?",
     "Everything is figreoutable",
     "What's your end game?",
-    "Challenges are what make life interesting and overcoming them is what makes life meaningful."
+    "Challenges are what make life interesting and overcoming them is what makes life meaningful.",
+    "He whos asks a question remains a fool for 5 minutes he who does not remains a fool forever."
   ];
   var randomItem = quote[Math.floor(Math.random() * quote.length)];
   document.getElementById("nextWord").innerHTML = randomItem;
